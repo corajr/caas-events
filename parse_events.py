4e1c7e1ddfeedfddd943912c0bcc4774650e480d
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2015-08-19 11:17:47 -0400

    add events and delete them

--- parse_events.py
+++ parse_events.py
@@ -4,11 +4,15 @@ import json
 import pytz
 import datetime
 import calendar
+import hashlib
 from nameparser import HumanName
 
 
 eastern = pytz.timezone('US/Eastern')
-
+def md5(x):
+    s = hashlib.md5()
+    s.update(x)
+    return s.hexdigest()
 
 def get_values(row):
     return map(lambda x: x.value, row)
@@ -21,13 +25,16 @@ def get_authors(row):
 
 
 def parse_workbook(filename):
-    out = {}
+    out = []
     book = xlrd.open_workbook(filename)
     sh = book.sheet_by_index(0)
     header = get_values(sh.row(0))
     for rx in range(1, sh.nrows):
         row = dict(zip(header, get_values(sh.row(rx))))
-        row['id'] = int(row['id'])
+        row['id'] = int(row['id']) if row['id'] else md5(row['title'])
+        if row['lecture-series'] == 'deleted':
+            out.append({'id': row['id'], 'action': 'delete'})
+            continue
         dt_xls = xlrd.xldate_as_tuple(row['datetime'], 0)
         dt = datetime.datetime(*dt_xls)
         dt = eastern.localize(dt)
@@ -35,10 +42,10 @@ def parse_workbook(filename):
         row['datetime'] = dt
         row['presenters'] = get_authors(row)
         del row['description']
-        out[row['id']] = row
+        out.append(row)
     return out
 
 if __name__ == '__main__':
     data = parse_workbook('New CAAS-Events.xls')
     with open('updated_events.json', 'wb') as f:
-        json.dump(data.values(), f, indent=4)
+        json.dump(data, f, indent=4)
--- scripts/add_events.php
+++ scripts/add_events.php
@@ -8,67 +8,72 @@ require_once("taxonomy_manage.php");
  */
 class Events_Command extends WP_CLI_Command {
 
-    /**
-     * Adds events to the database.
-     * 
-     * ## OPTIONS
-     * 
-     * <json>
-     * : The name of the JSON file to import.
-     * 
-     * ## EXAMPLES
-     * 
-     *     wp events add events.json
-     *
-     * @synopsis <json>
-     */
-    function add( $args, $assoc_args ) {
-        list( $filename ) = $args;
-        $js_str = file_get_contents($filename);
-        $json_a = json_decode($js_str, true);
+	/**
+	 * Adds events to the database.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <json>
+	 * : The name of the JSON file to import.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp events add events.json
+	 *
+	 * @synopsis <json>
+	 */
+	function add( $args, $assoc_args ) {
+		list( $filename ) = $args;
+		$js_str = file_get_contents($filename);
+		$json_a = json_decode($js_str, true);
 
-        foreach ($json_a as $event) {
-            $args = array( 'meta_key' => 'old-event-id',
-                           'meta_value' => $event['id'],
-                           'post_type' => 'event',
-            );
+		foreach ($json_a as $event) {
+			$args = array( 'meta_key' => 'old-event-id',
+						   'meta_value' => $event['id'],
+						   'post_type' => 'event',
+			);
 
-            $existed = false;
-            $posts = get_posts($args);
-            if (!empty($posts)) {
-                $post_id = $posts[0]->ID;
-                $existed = true;
-            } else {
-                $post_id = wp_insert_post(array(
-                    'post_type' => 'event',
-                    'post_title' => $event['title'],
-                    'post_excerpt' => $event['dek'],
-                    'post_content' => $event['description'],
-                    'post_status' => 'publish',
-                    'comment_status' => 'closed',
-                    'ping_status' => 'closed',
-                ));                
-            }
+			$existed = false;
+			$should_delete = $event['action'] == 'delete';
+			$posts = get_posts($args);
+			if (!empty($posts)) {
+				$post_id = $posts[0]->ID;
+				$existed = true;
+			} else if (!$should_delete) {
+				$post_id = wp_insert_post(array(
+					'post_type' => 'event',
+					'post_title' => $event['title'],
+					'post_excerpt' => $event['dek'],
+					'post_content' => $event['description'],
+					'post_status' => 'publish',
+					'comment_status' => 'closed',
+					'ping_status' => 'closed',
+				));
+			}
 
-            if ($post_id) {
-                update_post_meta($post_id, 'wpcf-event-date-time', $event['datetime']);
-                update_post_meta($post_id, 'wpcf-location', $event['location']);
-                update_post_meta($post_id, 'old-event-id', $event['id']);
-                if ($existed) {
-                    $updating = array(
-                        'ID' => $post_id,
-                        'post_title' => $event['title'],
-                        'post_excerpt' => $event['dek'],
-                    );
-                    wp_update_post($updating);
-                }
-                Authors::do_add_coauthors($post_id, $event);
-                Taxonomy::add_event_type($post_id, $event);
-            }
-        }
-    
-        WP_CLI::success( "$filename was successfully imported." );
-    }
+			if ($post_id) {
+				if ($existed && $should_delete) {
+					wp_delete_post($post_id);
+				} else {
+					update_post_meta($post_id, 'wpcf-event-date-time', $event['datetime']);
+					update_post_meta($post_id, 'wpcf-location', $event['location']);
+					update_post_meta($post_id, 'old-event-id', $event['id']);
+					if ($existed) {
+						$updating = array(
+							'ID' => $post_id,
+								'post_title' => $event['title'],
+								'post_excerpt' => $event['dek'],
+						);
+						wp_update_post($updating);
+					}
+					Authors::do_add_coauthors($post_id, $event);
+					Taxonomy::add_event_type($post_id, $event);
+				}
+			}
+		}
+
+		WP_CLI::success( "$filename was successfully imported." );
+	}
 }
 
 WP_CLI::add_command( 'events', 'Events_Command' );
